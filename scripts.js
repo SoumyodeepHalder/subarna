802f955c1a89fb8579157ec20d03d6bb0ddd39d3
Soumyodeep Halder <soumyodp@gmail.com>
2025-10-01 15:20:27 +0530

    responsive navbar

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const menu=document.querySelector('.menu');
+const navrightmob=document.querySelector ('.navrightmob');
+menu.addEventListener ('click', ()=>{
+    navrightmob.classList.toggle ('open');
+})
